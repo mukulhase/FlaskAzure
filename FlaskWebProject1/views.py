8f614fabe1e9314689d21136681230f4ee5cbb63
Mukul Hase <mukulhase@gmail.com>
2017-01-08 01:39:57 +0530

    second

--- FlaskWebProject1/views.py
+++ FlaskWebProject1/views.py
@@ -5,6 +5,16 @@ Routes and views for the flask application.
 from datetime import datetime
 from flask import render_template
 from FlaskWebProject1 import app
+import os
+from flask import Flask, request, redirect, url_for
+from werkzeug.utils import secure_filename
+import visionconnect
+
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+UPLOAD_FOLDER = os.path.join(APP_ROOT, 'static/uploads')
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 @app.route('/home')
@@ -35,3 +45,29 @@ def about():
         year=datetime.now().year,
         message='Your application description page.'
     )
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            hello = visionconnect.getTag(url_for('static', filename='uploads/' + filename))
+            return "Uploaded " + hello
+    return render_template(
+        'upload.html'
+            )
---
+++ FlaskWebProject1/visionconnect.py
@@ -0,0 +1,80 @@
+import httplib, urllib, base64, json, urllib2
+
+image_headers = {
+	# Request headers
+	'Content-Type': 'application/json',
+	'Ocp-Apim-Subscription-Key': 'a9c6c3ecc96646d0bea62e3839bb3a7e',
+}
+
+translation_headers = {
+	"Content-Type": "application/x-www-form-urlencoded",
+	"Accept": "application/xml",
+	'Ocp-Apim-Subscription-Key': '940a397d030044f299eceab7e9f9f99d',
+}
+
+params = urllib.urlencode({
+})
+
+# url of image
+def getTag(url):
+	try:
+		conn = httplib.HTTPSConnection('api.projectoxford.ai')
+		conn.request("POST", "/vision/v1.0/tag?%s" % params, "{\"url\": \"%s\"}" % url, image_headers)
+		response = conn.getresponse()
+		#response = request.get("https://api.cognitive.microsoft.com/sts/v1.0/issueToken", headers=translation_headers)
+		data = response.read()
+		#print(data)
+		conn.close()
+		parsed_json = json.loads(data)
+		tag = parsed_json['tags'][0]['name']
+		print tag
+		return tag
+	except Exception as e:
+		print(e)
+
+url = "http://www.ikea.com/PIAimages/0121010_PE277826_S5.JPG"
+# tag = getTag(url)
+tag = "plant"
+
+#token for translation API, 10 min life.
+def getToken():
+	try:
+		conn = httplib.HTTPSConnection('api.cognitive.microsoft.com')
+		conn.request("POST", "/sts/v1.0/issueToken",params, translation_headers)
+		response = conn.getresponse()
+		token = response.read()
+		# print(data)
+		conn.close()
+		return token
+	except Exception as e:
+		print(e)
+		return
+
+
+acc_token = getToken()
+# print(acc_token)
+
+appid = "Bearer" + " " + acc_token
+
+transParams = urllib.urlencode({
+	'appid': appid,
+	'text' : tag,
+	'from' : 'en-US',
+	'to' : 'hi',
+	'maxTranslations' : 1,
+})
+
+#from, to are language encodings, num = number of max translations
+def TranslateWord():
+	try:
+		conn = httplib.HTTPSConnection('api.microsofttranslator.com')
+		conn.request("POST", "/V2/Http.svc/GetTranslations?%s" % transParams, params, translation_headers)
+		response = conn.getresponse()
+		data = response.read()
+		print(data)
+		conn.close()
+		return
+	except Exception as e:
+		print(e)
+		return
+
