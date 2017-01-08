a2db9e0ae7aa829c6b55e75af5b55093b630e8c8
Sri Aurobindo Munagala <s.munagala@research.iiit.ac.in>
2017-01-08 13:56:48 +0530

    Meh 3

--- FlaskWebProject1/views.py
+++ FlaskWebProject1/views.py
@@ -86,7 +86,7 @@ def upload_URL():
         data = visionconnect.getTag(url)
         try:
             trans = visionconnect.TranslateWord(data)
-            return "Uploaded " + data + " " +  trans + " " + request.data.get("url")
+            return "Uploaded " + data + " " +  trans + " " + request
         except:
             trans = ''.join(traceback.format_stack())
         return "Uploaded " + data + " " + trans
