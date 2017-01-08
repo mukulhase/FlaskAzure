5dd312f4137cf238f8cfc3b2016e8388b230ac59
Mukul Hase <mukulhase@gmail.com>
2017-01-08 17:39:26 +0530

    tee

--- FlaskWebProject1/views.py
+++ FlaskWebProject1/views.py
@@ -87,33 +87,30 @@ def messenger_reply():
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
-	try:
-		if request.method == 'POST':
-			# check if the post request has the file part
-			if 'file' not in request.files:
-				flash('No file part')
-				return redirect(request.url)
-			file = request.files['file']
+	if request.method == 'POST':
+		# check if the post request has the file part
+		if 'file' not in request.files:
+			flash('No file part')
+			return redirect(request.url)
+		file = request.files['file']
 
-			# if user does not select file, browser also
-			# submit a empty part without filename
+		# if user does not select file, browser also
+		# submit a empty part without filename
 
-			if file.filename == '':
-				flash('No selected file')
-				return redirect(request.url)
-			if file and allowed_file(file.filename):
-				filename = secure_filename(file.filename)
-				file.save(os.path.join(app.config['UPLOAD_FOLDER'],
-						  filename))
-				data =  visionconnect.getTag('http://lifegivesyoulemons.azurewebsites.net/'+ url_for('static', filename='uploads/' + filename))
-				
-				return 'Uploaded ' + data + ' ' + trans
-				# out = "<object>" + data + "</object>" + " " + "<translatedObj>" + trans + "</translatedObj>"
-				# return Response(out, mimetype='text/xml')
-				# out = "<object>" + data + "</object>" + " " + "<translatedObj>" trans + "</translatedObj>"
-				# return Response(out, mimetype='text/xml')
-	except:
-		return ''.join(traceback.format_stack())
+		if file.filename == '':
+			flash('No selected file')
+			return redirect(request.url)
+		if file and allowed_file(file.filename):
+			filename = secure_filename(file.filename)
+			file.save(os.path.join(app.config['UPLOAD_FOLDER'],
+					  filename))
+			data =  visionconnect.getTag('http://lifegivesyoulemons.azurewebsites.net/'+ url_for('static', filename='uploads/' + filename))
+			
+			return 'Uploaded ' + data + ' ' + trans
+			# out = "<object>" + data + "</object>" + " " + "<translatedObj>" + trans + "</translatedObj>"
+			# return Response(out, mimetype='text/xml')
+			# out = "<object>" + data + "</object>" + " " + "<translatedObj>" trans + "</translatedObj>"
+			# return Response(out, mimetype='text/xml')
 	return render_template('upload.html')
 
 
