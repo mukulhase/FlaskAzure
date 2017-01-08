d89bb43ccc0cccbe9beab0a6d3af6e8049c0ad43
Sri Aurobindo Munagala <s.munagala@research.iiit.ac.in>
2017-01-08 16:59:57 +0530

    lasda

--- FlaskWebProject1/views.py
+++ FlaskWebProject1/views.py
@@ -73,9 +73,9 @@ def messenger_reply():
  			#conn = httplib.HTTPSConnection('evening-caverns-89101.herokuapp.com')
  			#conn.request("POST", "/sendAuro", json.dumps(data), headers)
 			data = urllib.urlencode({"message": "Lolol"})
-			h = httplib.HTTPSConnection('evening-caverns-89101.herokuapp.com')
-			h.request('POST', '/sendAuro', data, headers)
-			r = h.getresponse()
+			#h = httplib.HTTPSConnection('evening-caverns-89101.herokuapp.com')
+			#h.request('POST', '/sendAuro', data, headers)
+			#r = h.getresponse()
 			#r = "asd"
 			return r
 # 			return render_template('upload.html')
